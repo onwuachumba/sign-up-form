c7bfe07999475ed104afbcc90da8484172590818
regina onwuachumba <ginamartinsr@gmail.com>
2022-06-17 11:37:45 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document .getElementById ("form");
+const first = document .getElementById ("firstname");
+const last = document .getElementById ("lastname");
+const email = document .getElementById ("email");
+const password = document .getElementById ("password");
+
+form.addEventListener('submit',e =>{
+    e.preventDefault():
+
+});
