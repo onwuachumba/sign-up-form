740d3d2dc45f0ef0bb34566e12e28d4aae6c5ab8
regina onwuachumba <ginamartinsr@gmail.com>
2022-06-19 11:58:19 +0100

    corrected

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-const form = document .getElementById ("form");
-const first = document .getElementById ("firstname");
-const last = document .getElementById ("lastname");
-const email = document .getElementById ("email");
-const password = document .getElementById ("password");
+const form = document.getElementById ("form");
 
 form.addEventListener('submit',e =>{
-    e.preventDefault():
+    e.preventDefault();
+
+    const firstName = form ['firstname'].value;
+    const lastName = form ['lastname'].value;
+    const email = form ['email'].value;
+    const password = form ['password'].value;
+
+    if (firstName===''){
+        addErrorTo ('firstname', 'First Name cannot be empty');
+    }
+    if (lastName===''){
+        addErrorTo ('lastname', 'Last Name cannot be empty');
+    }
+    // console.log(isValid(email));
+    if (email===""){
+        addErrorTo('email','looks like this is not an email');
+    }
+    if (isValid(email) ){
+        
+        addErrorTo ('email', 'Email is not valid');
+    }
+    if (password===''){
+        addErrorTo ('password', 'password cannot be empty');
+    }
+
+
 
 });
+// let alarm=document.getElementById("alarm").style.opacity.value;
+// alarm =1
+// console.log(alarm)
+function addErrorTo(field, Message){
+    const small = form[field].parentNode.querySelector('small');
+    const alarms = form[field].parentNode.querySelectorAll('.alarm');
+    
+    small.innerText = Message
+    small.style.opacity ='1'
+    alarms.forEach(alarm => {
+        alarm.style.opacity = 1;
+    });
+}
+
+function isValid(email){
+    var re = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
+    return re.exec(email);
+        }
